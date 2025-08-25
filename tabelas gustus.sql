97b2357eb3b5b650085b8a7bc65099c919768c19
MARIANA MARIETTI DA COSTA <u24140@academico.cotuca.unicamp.br>
2025-08-25 12:53:20 -0300

    a

--- tabelas gustus.sql
+++ tabelas gustus.sql
@@ -1,4 +1,11 @@
 
+select * from gustus.usuarios
+select * from gustus.pratos
+select * from gustus.favoritos
+select * from gustus.degustados
+select * from gustus.wishlist
+
+
 create schema gustus;
 
 ---------------------------------------------
@@ -10,6 +17,15 @@ create table gustus.usuarios
     email varchar(150) unique not null,
     senha varchar(100) not null
 );
+
+--inserts
+insert into gustus.usuarios 
+(usuario, email, senha) 
+values
+('Mariana', 'mariana@email.com', '123456'),
+('Joao', 'joao@email.com', 'senha123'),
+('Ana', 'ana@email.com', 'segredo321');
+
 
 --adicionar, alterar e excluir--
 --add
@@ -37,6 +53,14 @@ create table gustus.pratos
     linkReceita varchar(max)
 );
 
+--inserts
+insert into gustus.pratos (prato, descricao, linkReceita) values
+('Risoto de Camarão', 'Prato italiano cremoso com camarões frescos.', 'https://www.tudogostoso.com.br/receita/185493-risoto-de-camarao-sem-frescura.html'),
+('Ratatouille', 'Receita francesa com legumes assados.', 'https://www.tudogostoso.com.br/receita/135302-ratatouille.html'),
+('File Mignon', 'Corte nobre de carne ao molho madeira.', 'https://www.tudogostoso.com.br/categorias/1116-file-mignon'),
+('Açaí', 'Sobremesa brasileira servida gelada.', 'https://www.tudogostoso.com.br/receita/296086-acai.html');
+
+
 --pagina inicial todos os produtos--
 
 --pagina do produto com favoritar, adicionar e remover da wishlist e experimentar--
@@ -51,6 +75,22 @@ create table gustus.favoritos
     idUsuario int not null foreign key references gustus.usuarios(idUsuario),
     idPrato int not null foreign key references gustus.pratos(idPrato)
 );
+
+--inserts
+-- Mariana favoritou Risoto e Açaí
+insert into gustus.favoritos (idUsuario, idPrato) values
+(1, 1),
+(1, 4);
+
+-- Joao favoritou Ratatouille e File Mignon
+insert into gustus.favoritos (idUsuario, idPrato) values
+(2, 2),
+(2, 3);
+
+-- Ana favoritou apenas Risoto
+insert into gustus.favoritos (idUsuario, idPrato) values
+(3, 1);
+
 
 --um usuario so pode ter 2 favoritos--
 select count(*) 
@@ -70,6 +110,20 @@ create table gustus.degustados
     nota int check (nota between 1 and 5),
     descricao text
 );
+
+--inserts
+-- Mariana degustou Ratatouille e avaliou
+insert into gustus.degustados (idUsuario, idPrato, nota, descricao) values
+(1, 2, 5, 'Muito saboroso, bem temperado.');
+
+-- Joao degustou Risoto
+insert into gustus.degustados (idUsuario, idPrato, nota, descricao) values
+(2, 1, 4, 'Bom, mas poderia ter mais camarão.');
+
+-- Ana degustou Açaí
+insert into gustus.degustados (idUsuario, idPrato, nota, descricao) values
+(3, 4, 5, 'Perfeito para dias quentes!');
+
 
 --pagina avaliar--
 
@@ -95,6 +149,20 @@ create table gustus.wishlist
     idUsuario int not null foreign key references gustus.usuarios(idUsuario),
     idPrato int not null foreign key references gustus.pratos(idPrato)
 );
+
+--inserts
+-- Mariana quer experimentar File Mignon
+insert into gustus.wishlist (idUsuario, idPrato) values
+(1, 3);
+
+-- Joao quer experimentar Açaí
+insert into gustus.wishlist (idUsuario, idPrato) values
+(2, 4);
+
+-- Ana quer experimentar Ratatouille
+insert into gustus.wishlist (idUsuario, idPrato) values
+(3, 2);
+
 
 --pagina wishlist do usuario--
 
